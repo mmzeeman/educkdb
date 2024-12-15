18ba0d25c1eb01252f9dc3c37030a94bce50b4cf
Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
2024-12-15 14:04:08 +0100

    Fixed another one... one to go

--- test/educkdb_test.erl
+++ test/educkdb_test.erl
@@ -1022,19 +1022,15 @@ float_and_double_extract2_test() ->
 
     {ok, R2} = educkdb:query(Conn, "insert into test values (1.0, 10.1), (2.0, 11.1), (3.0, 12.2);"),
     C2 = educkdb:get_chunk(R2, 0),
-    ?assertEqual( [ #{ type => bigint, data => [3] } ], educkdb:extract_chunk(C2)),
+
+    ?assertEqual( [ bigint ], educkdb:chunk_column_types(C2)),
+    ?assertEqual( [ [3] ], educkdb:chunk_columns(C2)),
 
     {ok, R3} = educkdb:query(Conn, "select * from test order by a;"),
     C3 = educkdb:get_chunk(R3, 0),
-    ?assertEqual(
-       [ #{ type => float,
-            data => [1.0, 2.0, 3.0] },
 
-         #{ type => double,
-            data => [10.1, 11.1, 12.2] }
-
-       ],
-       educkdb:extract_chunk(C3)),
+    ?assertEqual([ float, double ], educkdb:chunk_column_types(C3)),
+    ?assertEqual([[1.0, 2.0, 3.0], [10.1, 11.1, 12.2]], educkdb:chunk_columns(C3)),
 
     ok.
 
